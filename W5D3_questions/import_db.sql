d471485dcc6feb471cb2735124245711d8c37789
Elhoussine Elouardy <elouardy.elhoussine@gmail.com>
2022-01-26 11:15:58 -0800

    Add Replies Table

--- W5D3_questions/import_db.sql
+++ W5D3_questions/import_db.sql
@@ -16,9 +16,21 @@ DROP TABLE IF EXISTS question_follows;
 CREATE TABLE question_follows (
     id INTEGER PRIMARY KEY,
     user_id INTEGER NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id),
     question_id INTEGER NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (question_id) REFERENCES questions(id)
 )
 
 DROP TABLE IF EXISTS replies;
+CREATE TABLE replies(
+    id INTEGER PRIMARY KEY,
+    reply TEXT NOT NULL,
+    question_id INTEGER NOT NULL,
+    parent_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (parent_id) REFERENCES replies(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+)
