7b49fc4b24101c9681801318e0fb88d319ee57a9
ajoyvmon <ajoyvmon@gmail.com>
2022-01-26 11:35:16 -0800

    started insert statements

--- W5D3_questions/import_db.sql
+++ W5D3_questions/import_db.sql
@@ -1,16 +1,18 @@
+PRAGMA foreign_keys = ON;
+
 DROP TABLE IF EXISTS users;
 CREATE TABLE users(
     id INTEGER PRIMARY KEY, 
     fname VARCHAR(255),
     lname VARCHAR(255)
-    );
+);
 
 DROP TABLE IF EXISTS questions;
 CREATE TABLE questions (
     id INTEGER PRIMARY KEY,
     title VARCHAR(255),
     body TEXT NOT NULL
-)
+);
 
 DROP TABLE IF EXISTS question_follows;
 CREATE TABLE question_follows (
@@ -20,7 +22,7 @@ CREATE TABLE question_follows (
 
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (question_id) REFERENCES questions(id)
-)
+);
 
 DROP TABLE IF EXISTS replies;
 CREATE TABLE replies(
@@ -33,4 +35,25 @@ CREATE TABLE replies(
     FOREIGN KEY (question_id) REFERENCES questions(id),
     FOREIGN KEY (parent_id) REFERENCES replies(id),
     FOREIGN KEY (user_id) REFERENCES users(id),
-)
+);
+
+DROP TABLE IF EXISTS question_likes;
+CREATE TABLE question_likes (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+INSERT INTO
+    users(fname, lname)
+VALUES
+    ("Elhoussine", "Elouardy"),
+    ("Abigail", "Montemayor");
+
+INSERT INTO
+    questions(title, body)
+VALUES
+    ("Lunch", "What's for lunch?")
