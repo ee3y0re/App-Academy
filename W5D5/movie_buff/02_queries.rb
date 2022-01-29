e5ed62cb3dfb640bd0a5cfa22f4ff563e2faf3fb
ajoyvmon <ajoyvmon@gmail.com>
2022-01-28 17:02:46 -0800

    Need to resolve cast list

--- W5D5/movie_buff/02_queries.rb
+++ W5D5/movie_buff/02_queries.rb
@@ -20,7 +20,11 @@ end
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
-  
+  Actor
+    .select(:id, :name)
+    .join(:movies)
+    .where(movies: {title: title})
+    .order('castings.ord')
 end
 
 def vanity_projects
