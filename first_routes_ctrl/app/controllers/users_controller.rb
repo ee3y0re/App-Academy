88c99ccd7570de7a4c2d3f4cfaea102e82017b4b
ajoyvmon <ajoyvmon@gmail.com>
2022-02-02 14:47:25 -0800

    created index, create, and show

--- first_routes_ctrl/app/controllers/users_controller.rb
+++ first_routes_ctrl/app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
-
+    def index
+        render plain: "Whatever we want in index"
+    end
     
+    def create
+        render json: params
+    end
+
+    def show
+        render json: params
+    end
 end
