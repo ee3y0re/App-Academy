91e4d87211183f52dbe557b8de7509d67c3912e0
ajoyvmon <ajoyvmon@gmail.com>
2022-02-02 16:51:35 -0800

    completed and tested create

--- first_routes_ctrl/app/controllers/users_controller.rb
+++ first_routes_ctrl/app/controllers/users_controller.rb
@@ -6,10 +6,22 @@ class UsersController < ApplicationController
     end
     
     def create
-        render json: params
+        user = User.new(user_params)
+        # replace the `user_attributes_here` with the actual attribute keys
+        if user.save!
+			render json: user
+		else
+			render json: user.error.full_messages, status:422
+		end
     end
 
     def show
         render json: params
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email)
     end
 end
