ed7154de908b49cd3e4c69fa9deb0a5f5d07806b
ajoyvmon <ajoyvmon@gmail.com>
2022-02-03 09:16:36 -0800

    resources exceptions

--- W6D3_first_routes_ctrl/config/routes.rb
+++ W6D3_first_routes_ctrl/config/routes.rb
@@ -5,11 +5,13 @@ Rails.application.routes.draw do
   get  'users', to: 'users#index', as: 'users'
   get 'users/:id', to: 'users#show', as: 'user'
   post  'users', to: 'users#create'
-  get 'users/new', to: 'users#new', as: 'new_user'
-  get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
+  # get 'users/new', to: 'users#new', as: 'new_user'
+  # get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
   patch  'users/:id', to: 'users#update' #update specific user
-  put 'users/:id', to: 'users#update'
+  # put 'users/', to: 'users#update'
   delete 'users/:id', to: 'users#destroy'
+
+  # resources :users, except: [:new, :edit]
 
 
 
