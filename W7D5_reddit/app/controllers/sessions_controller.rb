2d9b15a0bb74c54f9d1d5df271af4a19a7098b45
ajoyvmon <ajoyvmon@gmail.com>
2022-02-11 16:36:55 -0800

    users html end and sessions html start

--- W7D5_reddit/app/controllers/sessions_controller.rb
+++ W7D5_reddit/app/controllers/sessions_controller.rb
@@ -1,2 +1,28 @@
 class SessionsController < ApplicationController
+    def new
+        render :new
+    end
+
+    def create #logging in user
+        @user = User.find_by_credentials(
+            params[:user][:username],
+            params[:user][:password]
+        )
+
+        if @user
+            login!(@user)
+            redirect_to subs_url
+        else
+            flash[:errors] = ["Invalid username or password "]
+            render :new
+        end
+    end
+
+    def destroy
+        if logged_in?
+            logout!
+        else
+            redirect_to new_session_url
+        end
+    end
 end
