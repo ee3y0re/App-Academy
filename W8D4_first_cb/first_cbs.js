22c834ddc538948863b0cb4a37709f2ef59226f6
Nick Pietrow <njpietrow@gmail.com>
2022-02-17 14:38:55 -0800

    added add numbers function

--- W8D4_first_cb/first_cbs.js
+++ W8D4_first_cb/first_cbs.js
@@ -45,4 +45,35 @@ class Clock {
     }
 }
 
+/*
 const clock = new Clock();
+*/
+
+const readline = require('readline');
+
+reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function addNumbers(sum, numsLeft, completionCallback){
+    if (numsLeft === 0 ){
+        completionCallback(sum);
+        reader.close();
+        // return;
+    } else {
+        reader.question("Give me a number: ", answer => {
+            let num = parseInt(answer);
+            let newSum = sum + num;
+            console.log(newSum);
+            let newNumsLeft = numsLeft - 1;
+            addNumbers(newSum, newNumsLeft, completionCallback);
+        });
+    }
+};
+
+/* 
+addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
+ */
+
+
