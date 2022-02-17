765f72b9cbf951637e124692521fad988e020612
Nick Pietrow <njpietrow@gmail.com>
2022-02-17 15:40:09 -0800

    added ask if greater than and inner bubblesort

--- W8D4_first_cb/first_cbs.js
+++ W8D4_first_cb/first_cbs.js
@@ -2,7 +2,7 @@
 //     setInterval,
 // } from 'timers/promises';
 
-class Clock {
+/* class Clock {
     constructor() {
         let date = new Date();
         this.hours = date.getHours();
@@ -43,41 +43,48 @@ class Clock {
         // 1. Increment the time by one second.
         // 2. Call printTime.
     }
-}
+} */
 
 /*
 const clock = new Clock();
 */
 
-// const readline = require('readline');
+/* 
+=============================================
+*/
 
-// reader = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
+/* const readline = require('readline');
 
-// function addNumbers(sum, numsLeft, completionCallback){
-//     if (numsLeft === 0 ){
-//         completionCallback(sum);
-//         reader.close();
-//         // return;
-//     } else {
-//         reader.question("Give me a number: ", answer => {
-//             let num = parseInt(answer);
-//             let newSum = sum + num;
-//             console.log(newSum);
-//             let newNumsLeft = numsLeft - 1;
-//             addNumbers(newSum, newNumsLeft, completionCallback);
-//         });
-//     }
-// };
+reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function addNumbers(sum, numsLeft, completionCallback){
+    if (numsLeft === 0 ){
+        completionCallback(sum);
+        reader.close();
+        // return;
+    } else {
+        reader.question("Give me a number: ", answer => {
+            let num = parseInt(answer);
+            let newSum = sum + num;
+            console.log(newSum);
+            let newNumsLeft = numsLeft - 1;
+            addNumbers(newSum, newNumsLeft, completionCallback);
+        });
+    }
+}; */
 
 /* 
 addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
  */
 
+/* 
+=============================================
+*/
 
-Function.prototype.myBind = function(context) {
+/* Function.prototype.myBind = function(context) {
     return () => { //returns function captures this and context
         //this = whatever function calls myBind
         debugger
@@ -104,4 +111,72 @@ const boundTurnOn = turnOn.bind(lamp);
 const myBoundTurnOn = turnOn.myBind(lamp);
 
 boundTurnOn(); // should say "Turning on a lamp"
-myBoundTurnOn(); // should say "Turning on a lamp"
+myBoundTurnOn(); // should say "Turning on a lamp" */
+
+/* 
+=============================================
+*/
+
+const readline = require("readline");
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Write this first.
+function askIfGreaterThan(el1, el2, callback) {
+    // Prompt user to tell us whether el1 > el2; pass true back to the
+    // callback if true; else false.
+    reader.question(`Is ${el1} > ${el2} ? `, response => {
+        if (response === "yes") {
+            callback(true);
+        } else {
+            callback(false);
+        }
+    });
+}
+
+// askIfGreaterThan(1,2,function(status){ console.log("exceuted " + status)});
+
+// Once you're done testing askIfGreaterThan with dummy arguments, write this.
+function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
+    // Do an "async loop":
+    // 1. If (i == arr.length - 1), call outerBubbleSortLoop, letting it
+    //    know whether any swap was made.
+    // 2. Else, use `askIfGreaterThan` to compare `arr[i]` and `arr[i +
+    //    1]`. Swap if necessary. Call `innerBubbleSortLoop` again to
+    //    continue the inner loop. You'll want to increment i for the
+    //    next call, and possibly switch madeAnySwaps if you did swap.
+    if (i === arr.length - 1) {
+        outerBubbleSortLoop(madeAnySwaps);
+        console.log(arr);
+    } else {
+        askIfGreaterThan(arr[i], arr[i+1], (isGreaterThan) => {
+            if (isGreaterThan) {
+                [arr[i], arr[i+1]] = [arr[i+1], arr[i]];
+                madeAnySwaps = true;
+            }
+            innerBubbleSortLoop(arr, i+1, madeAnySwaps, outerBubbleSortLoop);
+        });
+    }
+}
+
+innerBubbleSortLoop([4,3,2,1], 0, false, function() {console.log("In outer bubble sort")});
+
+// Once you're done testing innerBubbleSortLoop, write outerBubbleSortLoop.
+// Once you're done testing outerBubbleSortLoop, write absurdBubbleSort.
+
+function absurdBubbleSort(arr, sortCompletionCallback) {
+  function outerBubbleSortLoop(madeAnySwaps) {
+    // Begin an inner loop if you made any swaps. Otherwise, call
+    // `sortCompletionCallback`.
+  }
+
+  // Kick the first outer loop off, starting `madeAnySwaps` as true.
+}
+
+/* absurdBubbleSort([3, 2, 1], function(arr) {
+  console.log("Sorted array: " + JSON.stringify(arr));
+  reader.close();
+}); */
