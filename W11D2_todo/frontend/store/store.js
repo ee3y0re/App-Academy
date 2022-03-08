7a4c00208415a7a903f6c6e8be853f3827346e4e
ajoyvmon <ajoyvmon@gmail.com>
2022-03-08 15:38:29 -0800

    todo and todos actions and reducers

--- W11D2_todo/frontend/store/store.js
+++ W11D2_todo/frontend/store/store.js
@@ -1,10 +1,8 @@
-import {
-    createStore
-} from "redux";
+import { createStore } from "redux";
 import rootReducer from "../reducers/root_reducer";
 
 const configureStore = (preloadedState = {}) => {
-    return createStore(rootReducer, preloadedState);
+  return createStore(rootReducer, preloadedState);
 }
 
 export default configureStore;
--- W11D2_todo/frontend/todo_redux.jsx
+++ W11D2_todo/frontend/todo_redux.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from "./store/store";
+import { receiveTodo, receiveTodos } from "./actions/todo_actions"
 
 document.addEventListener("DOMContentLoaded", () => {
   const content = document.getElementById("content");
   const test = <h1>Todos App</h1>;
   ReactDOM.render(test, content);
+
+  const store = configureStore();
+  window.store = store;
+  window.receiveTodo = receiveTodo;
+  window.receiveTodos = receiveTodos;
 });
